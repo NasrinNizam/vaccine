52d808ca02ecf20856f96e93c5123f37867fc49f
Nasrin Sultana <168544785+NasrinNizam@users.noreply.github.com>
2024-10-30 22:20:30 +0600

    add brands logo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Banner } from '../Banner/Banner'
+import { Brand } from '../brands/Brand'
 
 export const Home = () => {
   return (
     <div>
       <Banner />
+      <Brand />
     </div>
   )
 }
